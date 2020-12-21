127c93a150f9d227dacf8da830a464c058a76f00
Minnan <978312456a@gmail.com>
2020-12-21 23:05:52 +0800

    MyBatisPlus分页配置

---
+++ common/src/main/java/site/minnan/rental/infrastructure/config/MyBatisPlusConfig.java
@@ -0,0 +1,17 @@
+package site.minnan.rental.infrastructure.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+
+}
