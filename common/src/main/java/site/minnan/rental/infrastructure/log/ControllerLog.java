39074f2605c90dc13940ba843ab015811890b761
Minnan <978312456a@gmail.com>
2020-12-27 23:31:56 +0800

    引入AOP

---
+++ common/src/main/java/site/minnan/rental/infrastructure/log/ControllerLog.java
@@ -0,0 +1,11 @@
+package site.minnan.rental.infrastructure.log;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class ControllerLog {
+}
