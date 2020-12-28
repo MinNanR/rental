3d28eee984dc4e1eed7242ec3af391fbc32add0f
MinNanR <978123456a@gmail.com>
2020-12-28 09:34:49 +0800

    日志AOP

--- common/src/main/java/site/minnan/rental/infrastructure/log/ControllerLog.java
+++ common/src/main/java/site/minnan/rental/infrastructure/log/ControllerLog.java
@@ -1,11 +1,48 @@
 package site.minnan.rental.infrastructure.log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Aspect
 @Component
 @Slf4j
 public class ControllerLog {
+
+    @Pointcut("execution(public * site.minnan.rental.userinterface.fascade..*..*(..))")
+    private void controllerLog() {
+    }
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Around("controllerLog()")
+    public Object logAroundController(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long time = System.currentTimeMillis();
+        Object[] args = proceedingJoinPoint.getArgs();
+        List<String> argStringList = Arrays.stream(args).map(Object::toString).collect(Collectors.toList());
+        String argsString = objectMapper.writeValueAsString(argStringList);
+        String methodFullName = proceedingJoinPoint.getTarget().getClass().getName()
+                + "." + proceedingJoinPoint.getSignature().getName();
+        log.info("controller调用{}，参数：{}", methodFullName, argsString);
+        Object retValue = null;
+        try {
+            retValue = proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            log.error("调用接口异常", throwable);
+        }
+        time = System.currentTimeMillis() - time;
+        String responseString = objectMapper.writeValueAsString(retValue);
+        log.info("controller调用{}完成，返回数据:{}，用时{}ms", methodFullName, responseString, time);
+        return retValue;
+    }
 }
